ccef44ace481d30f4036ead1e7bf4e895ced9d03
Jonathan Lin <jonathan9800@gmail.com>
2018-03-31 15:44:00 -0700

    Added functionality to calculate number of connected components

--- connected_components.py
+++ connected_components.py
@@ -3,10 +3,39 @@ from graph_constructor import GraphConstructor
 
 class ConnectedComponents:
     def __init__(self, file_path):
-        constructor = GraphConstructor()
-        graph = constructor.construct_graph(file_path)
+        self.constructor = GraphConstructor()
+        self.graph = self.constructor.construct_graph(file_path, directed=False)
 
     # returns the number of connected components in the graph
-    def connected_components(self):
-        # TODO
-        return
+    def num_connected_components(self):
+        seen = set()
+        num_components = 0
+        for vertex in self.graph.keys():
+            if vertex not in seen:
+                self.bfs(vertex, seen)
+                num_components += 1
+        return num_components
+
+    # breadth first search graph traversal
+    def bfs(self, head, seen):
+        queue = [head]
+        while queue:
+            vertex = queue.pop(0)
+            for neighbor in self.graph[vertex]:
+                if neighbor not in seen:
+                    queue.append(neighbor)
+            seen.add(vertex)
+
+
+def main():
+    cc = ConnectedComponents("graphs/n100.txt")
+    num_components = cc.num_connected_components()
+    output = "This graph has " + str(num_components) + " connected component"
+    if num_components != 1:
+        output += "s"
+
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
--- graph_constructor.py
+++ graph_constructor.py
@@ -1,6 +1,3 @@
-from pprint import PrettyPrinter
-
-
 class GraphConstructor:
     # initializes the graph given the number of vertices
     @staticmethod
@@ -10,7 +7,7 @@ class GraphConstructor:
 
     # constructs and returns a graph given the graph input file
     # assumes input file is well formed
-    def construct_graph(self, file_path):
+    def construct_graph(self, file_path, directed=False):
         graph = {}
         graph_file = open(file_path, "r")
         num_vertices = int(graph_file.readline())
@@ -19,18 +16,10 @@ class GraphConstructor:
         for edge in graph_file:
             vertex_a, vertex_b = (int(vertex) for vertex in edge.split())
             # usage of set takes care of possible duplicates
-            graph[vertex_a].add(vertex_b)
-            graph[vertex_b].add(vertex_a)
+            if directed:
+                graph[vertex_a].add(vertex_b)
+            else:
+                graph[vertex_a].add(vertex_b)
+                graph[vertex_b].add(vertex_a)
 
         return graph
-
-
-def main():
-    constructor = GraphConstructor()
-    graph = constructor.construct_graph("graphs/n10.txt")
-    pp = PrettyPrinter(indent=4)
-    pp.pprint(graph)
-
-
-if __name__ == "__main__":
-    main()
